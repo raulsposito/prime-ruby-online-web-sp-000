88fbe9293933bf860bd8bbf17e42387a737a4a54
Raul Sposito <raulsposito@gmail.com>
2019-07-26 15:23:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,3 @@
-# Add  code here!
-#array [0..1000]
-#def prime?(array)
-#  array.each do |i|
-#  if (i / i ) && (i / 1 ) == 1 || i 
-#    return true
-#  else false
-#  end
-#end
-
 def prime?(number)
   if number >= 2
     (2..number - 1).all? do |x|
